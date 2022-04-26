c1fcbc721d5bf202ff4a94e3e35eecd5558f542a
Alexandros Dimas <alexandrosdms98@gmail.com>
2022-04-26 15:05:43 +0300

    remove unecessary prompt

--- matlab/runcalc.m
+++ matlab/runcalc.m
@@ -1,12 +1,17 @@
+% Alexandros Dimas
+% Department of Mechanical Enginnering & Aeronautics
+% University of Patras
+% Applied Mechanics Laboratory
+% Finite Element Method II
+% Lab Assignment 1
+% Spring 2022
+%
+% This the main routine of the code. The necessary plots are produced
+% only for option 4 [All] of the prompt message.
+
 clc, clear, close all
 
 addpath('utils')
-
-% params = getBeamParams;
-prompt_mode = sprintf(['Chose Mode for the Calculations:',...
-                        '\nUser Mode :\t\t1',...
-                        '\nAssignment Mode: \t2',...
-                        '\n\nEnter your Option ~> ']);
 
 prompt_sol = sprintf(['What is the desired solution?',...
                       '\nEuler\t\t: 1', ...
@@ -15,35 +20,16 @@ prompt_sol = sprintf(['What is the desired solution?',...
                       '\nAll\t\t: 4'...
                       '\n\nEnter your Option ~> ']);
 
-persistent userMode, assignMode, sol;
+
 while true
-    prompt_mode;
-    mode = input(prompt_mode);
-    if mode == 1
-        userMode = true;
-        assignMode = false;
-        fprintf('\nEntering User Mode...\n');
-        while true
-        prompt_sol;
-        sol = input(prompt_sol);
-        if sol == 1 || sol == 2 || sol == 3 || sol == 4
-            break;
-        else
-            fprintf('\nERROR: %d is not a viable choice, try again.\n', sol);
-        end
-        end
-        break;
-    elseif mode == 2
-        assignMode = true;
-        userMode = false;
-        fprintf('\nEntering Assignment Mode...\n');
+    prompt_sol;
+    sol = input(prompt_sol);
+    if sol == 1 || sol == 2 || sol == 3 || sol == 4
         break;
     else
-        fprintf('\nERROR: %d is not a viable choice, try again\n', mode);
+        fprintf('\nERROR: %d is not a viable choice, try again.\n', sol);
     end
-
-end
-        
+end        
 
 
 [L_h_ratio, w_midpoint, theta_midpoint] = calculations(sol)
