b40d639903113abd1f26d77adf419c451e17eb73
Alexandros Dimas <alexandrosdms98@gmail.com>
2022-04-26 13:53:24 +0300

    Reaction Forces are not needed, the correspondin DOFs are zero

--- matlab/utils/getNodalLoads.m
+++ matlab/utils/getNodalLoads.m
@@ -16,16 +16,14 @@ L = params.L;
 
 while node <= elementsNo + 1
     if node == 1
-        nodalLoads(1,i:i+1) = [R_A M_A];
         node = node + 1;
         i = i + 2;
+        continue;
     elseif mod(elementsNo,2) == 0 && node == (elementsNo/2) + 1
         nodalLoads(1,i:i+1) = nodalLoads(1,i:i+1) + [load 0];
         node = node + 1;
         i = i + 2;
     elseif node == elementsNo + 1
-        disp('node 5')
-        nodalLoads(1,i:i+1) = nodalLoads(1,i:i+1) + [R_B M_B];
         node = node + 1;
         i = i + 2;
     else
