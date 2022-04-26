46601c5e9e87ba641f3529cdc12855d11965a322
Alexandros Dimas <alexandrosdms98@gmail.com>
2022-04-26 12:11:50 +0300

    Total stiffness matrix generator

---
+++ matlab/utils/defStiffnessMat.m
@@ -0,0 +1,22 @@
+function [K_Total] = defStiffnessMat(Ke, params)
+%UNTITLED3 Summary of this function goes here
+%   Detailed explanation goes here
+
+elementsNo = params.elementsNo;
+% elementsNo = 4;
+
+totalDOF = 2*(elementsNo + 1);
+K_Total = zeros(totalDOF);
+
+for element = 1:elementsNo
+    if element == 1
+        row = 1; column = 1;
+    else
+        row = row + 2; column = column + 2;
+    end
+    K_Total(row:row+3,column:column+3) = ...
+        K_Total(row:row+3,column:column+3) + Ke;
+    
+end
+end
+
